0204502bc2f85ef16c5bbbad2ca5b50146f6c2f6
Hosszu Aron <aron.hosszu@accesa.eu>
2018-09-04 11:51:53 +0300

    JPA Integration

--- src/test/java/com/workspace/testproject/rest/spring/RestSpringTest.java
+++ src/test/java/com/workspace/testproject/rest/spring/RestSpringTest.java
@@ -17,21 +17,21 @@ public class RestSpringTest {
     private SpringRequestClient springRequestClient;
 
     @Test
-    public void getRestTest() {
+    public void restGetTest() {
         ResponseEntity<TodosModel> response = springRequestClient.getDummy(TodosModel.class);
         Assert.assertEquals("1", response.getBody().getId());
         Assert.assertEquals(200, response.getStatusCodeValue());
     }
 
     @Test
-    public void postRestTest() {
+    public void restPostTest() {
         ResponseEntity<TodosModel> response = springRequestClient.postDummy();
         Assert.assertEquals("5", response.getBody().getUserId());
         Assert.assertEquals(201, response.getStatusCodeValue());
     }
 
     @Test
-    public void putRestTest() {
+    public void restPutTest() {
         ResponseEntity<TodosModel> response = springRequestClient.putDummy();
         Assert.assertEquals("7", response.getBody().getUserId());
         Assert.assertEquals(200, response.getStatusCodeValue());
